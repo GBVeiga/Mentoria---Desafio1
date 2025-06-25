9fc10a5cc65b4aec83fb47b516c8156ae4d3c2b6
Gabriel Veiga <gabriel.trezena@hotmail.com.br>
2025-06-25 20:54:49 -0300

    Adaptando o código ao Clean Code.

---
+++ src/config/settings.py
@@ -0,0 +1,8 @@
+SETTINGS = {
+    "file_path": "mentoria/Mentoria---Desafio1/data/gupy.json",
+    "lista_vagas": [
+        "analista de dados",
+        "engenheiro de dados",
+        "engenheiro de dados júnior",
+    ]
+}
